da428b8f3df6c06d922604ea20bd176386f5253b
Kasey Jones <kaseyjones49@gmail.com>
2022-05-12 14:29:51 -0500

    created weapons class for robot and tested that it does run correctly

---
+++ main.py
@@ -0,0 +1,5 @@
+from weapon import Weapon
+
+
+weapon_one = Weapon("Dagger", 25)
+print(weapon_one.attack_power, weapon_one.name)
---
+++ weapon.py
@@ -0,0 +1,5 @@
+
+class Weapon: 
+    def __init__(game, name, attack_power):
+           game.name = name
+           game.attack_power = attack_power
